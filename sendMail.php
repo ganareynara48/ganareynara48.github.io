8aec35722032033d5f8374cc48fa40a064eb25dd
gana_ibs <gana.reynara@ibstower.com>
2022-11-27 16:45:59 +0700

    ss

--- sendMail.php
+++ sendMail.php
@@ -1,47 +1,30 @@
 <?php
+    ini_set( 'display_errors', 1 );   
+    error_reporting( E_ALL );    
 
-include('./phpmailer/classes/class.phpmailer.php');
+    $name   = ($_POST['name']!=NULL) ? $_POST['name'] : "";
+    $email  = ($_POST['email']!=NULL) ? $_POST['email'] : "";
+    $phone  = ($_POST['phone']!=NULL) ? $_POST['phone'] : "";
+    $help   = ($_POST['help']!=NULL) ? $_POST['help'] : "";
 
-$name   = ($_POST['name']!=NULL) ? $_POST['name'] : "";
-$email  = ($_POST['email']!=NULL) ? $_POST['email'] : "";
-$phone  = ($_POST['phone']!=NULL) ? $_POST['phone'] : "";
-$help   = ($_POST['help']!=NULL) ? $_POST['help'] : "";
+    $branding           = ($_POST['branding']!=NULL) ? $_POST['branding'] : "";
+    $social_media       = ($_POST['social_media']!=NULL) ? $_POST['social_media'] : "";
+    $digital_marketing  = ($_POST['digital_marketing']!=NULL) ? $_POST['digital_marketing'] : "";
+    $others             = ($_POST['others']!=NULL) ? $_POST['others'] : "";
 
-$branding           = ($_POST['branding']!=NULL) ? $_POST['branding'] : "";
-$social_media       = ($_POST['social_media']!=NULL) ? $_POST['social_media'] : "";
-$digital_marketing  = ($_POST['digital_marketing']!=NULL) ? $_POST['digital_marketing'] : "";
-$others             = ($_POST['others']!=NULL) ? $_POST['others'] : "";
 
-$penerima   = "reynaragana@gmail.com";    
-$subjek     = "I Need Your Help~";
-$pesan      = "Need : " . $help . "\n" .
-                "Services : " . $branding . $social_media . $digital_marketing . $others;  
-$headers    = "From :" . $email;
+    $subject     = "I Need Your Help~";
+    $message      = "Need : " . $help . "\n" .
+                    "Services : " . $branding . $social_media . $digital_marketing . $others . "\n" .
+                    "Name :" . $name . "\n" .
+                    "Email :" . $email . "\n" .
+                    "Phone :" . $phone . "\n";  
 
-$mail = new PHPMailer;
+    $headers    = "From :" . $email;
+   
+    $to = "thrivedesignagency@domain.com";
 
-$mail->IsSMTP();
-
-$mail->SMTPSecure = 'ssl';
-
-$mail->Host = "smtp.gmail.com"; //hostname masing-masing provider email
-$mail->SMTPDebug = 2;
-$mail->Port = 465;
-$mail->SMTPAuth = true;
-
-$mail->Timeout = 60; // timeout pengiriman (dalam detik)
-$mail->SMTPKeepAlive = true; 
-
-$mail->Username = "ganareynara@gmail.com"; //user email
-$mail->Password = "Gana6699"; //password email
-
-$mail->SetFrom($email,"From : "); //set email pengirim
-
-$mail->Subject = $subjek; //subyek email
-
-$mail->AddAddress("reynaragana@gmail.com","To : "); //tujuan email
-
-$mail->MsgHTML($pesan);
-
-if($mail->Send()) echo json_encode(array("statusCode"=>200));
-else echo "Failed to sending message";
+    mail($to,$subject,$message, $headers);
+        
+    echo json_encode(array("statusCode"=>200));
+?>
